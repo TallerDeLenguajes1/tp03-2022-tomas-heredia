028f3d6d9d2ec76ff2153a7820bc090f8a2d2572
Nombre <correo@correo.com>
2022-04-07 21:31:52 -0300

    punto 1 correjido-2

--- 1.c
+++ 1.c
@@ -4,28 +4,29 @@
 
 void main(){
     char *V[5];
+    char *Buff= (char *) malloc(100*sizeof(char));
     
-    for (int i = 0; i < 5; i++)
-    {
-        V[i] = (char *)malloc(20*sizeof(char));
 
-    }
     
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)   //ingresa nombres
     {
+        
        printf("\n ingrese el nombre %d: ",i+1);
-       gets(V[i]);
+       gets(Buff);
+       V[i]= (char *) malloc((strlen(Buff)+1)*sizeof(char));
+       strcpy(V[i],Buff);
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++) //muestro nombres
     {
         printf("\n El nombre %d es: ",i+1);
         puts(V[i]);
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)//libero memoria
     {
         free(V[i]);
 
     }
+    free(Buff);
 }
